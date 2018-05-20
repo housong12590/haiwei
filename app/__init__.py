84cd0b53ccc397b6482243154da078089d8e3d5f
hous <304536797@qq.com>
2018-05-20 22:27:06 +0800

    123

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
 migrate = Migrate()
+bootstrap = Bootstrap()
 
 
 def create_app():
@@ -12,6 +14,7 @@ def create_app():
     app.config.from_object(Config)
     db.init_app(app)
     migrate.init_app(app, db)
+    bootstrap.init_app(app)
 
     from .build import build as build_bp
     app.register_blueprint(build_bp, url_prefix='/build')
