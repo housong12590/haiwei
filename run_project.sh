c24c37d893a4de434ff23629bddf378c682a5477
hous <304536797@qq.com>
2018-05-27 00:53:42 +0800

    修改端口号

--- run_project.sh
+++ run_project.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-PROJECT_NAME=haiwei1
+PROJECT_NAME=tx_ops
 
 docker rm -f ${PROJECT_NAME}
 docker rmi -f ${PROJECT_NAME}
 docker build -t ${PROJECT_NAME} .
-docker run -d --name ${PROJECT_NAME} -p 8024:5000 ${PROJECT_NAME}
+docker run -d --name ${PROJECT_NAME} -p 8023:5000 ${PROJECT_NAME}
 
 
 
