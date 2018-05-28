03010d8ee6467c5cc8658df1b305af38d62a9556
hous <304536797@qq.com>
2018-05-28 23:12:30 +0800

    table

---
+++ migrations/2018_05_28_123424_create_environs_table.py
@@ -0,0 +1,24 @@
+from orator.migrations import Migration
+
+
+class CreateEnvironsTable(Migration):
+
+    def up(self):
+        """
+        Run the migrations.
+        """
+        with self.schema.create('environs') as table:
+            table.increments('id')
+            table.string('name')
+            table.string('key')
+            table.string('value').default('')
+            table.string('desc').nullable()
+            table.integer('parent_id', unsigned=True)
+            table.integer('project_id', unsigned=True)
+            table.timestamps()
+
+    def down(self):
+        """
+        Revert the migrations.
+        """
+        self.schema.drop('environs')
---
+++ models/project.py
@@ -0,0 +1,6 @@
+from orator import Model
+
+
+class Project(Model):
+
+    pass
